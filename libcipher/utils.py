6bb9bd35cdc6ce9c1d30096e07957183587ceeca
Anupama-Github <anupama.2312.bmsit@gmail.com>
2015-04-19 17:10:12 +0530

    Fixed path error

--- libcipher/utils.py
+++ libcipher/utils.py
@@ -11,7 +11,7 @@ class EnglishChecker():
        
         self.LETTERS_AND_SPACE = string.ascii_letters + string.whitespace
         #f = open('/home/travis/build/Anupama-Github/libcipher/libcipher/dictionary.txt')
-        f = open('/home/mer/Downloads/libcipher/libcipher/dictionary.txt')
+        f = open('/home/travis/build/Chennaipy/libcipher/libcipher')
         self.english_words = {}
         for word in f.read().split('\n'):
             self.englishWords[word] = None
