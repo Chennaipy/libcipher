02198c13d85d1a98650c15407aad47316399240f
rajeshkumargp <gprkumar@gmail.com>
2015-04-20 01:17:24 +0530

    Encryption Function is updated
    Encryption function is coded to handle empty strings,and exception
    thrown for InvalidKeySize.

--- libcipher/transpos.py
+++ libcipher/transpos.py
@@ -1,35 +1,56 @@
-""" 
+"""Transposition cipher implementation
 
-Transposition cipher implementation.
-
-Provides functions to encrypt / decrypt a message by transpose ciphering it.
-
+Provides functions to encrypt/decrypt a message by transpose ciphering it.
 Encryption needs a key(a number).
-
 """
 
 
-def encrypt(keysize,message):
+class InvalidKeySizeException(Exception):
+    def __init__(self, keysize, messagesize):
+        self.keysize = keysize
+        self.messagesize = messagesize
+
+    def __str__(self):
+        return("Cipher key is limited to half the length of the message" + 
+        " size.\nKey Size is " + repr(self.keysize) + 
+        " and Message Size is " + repr(self.messagesize))
+
+
+def encrypt(keysize, message):
     '''
     Args :
         Transposition Cipher Key Size,Message
     Returns:
-        message encrypted using transposition cipher
+        message encrypted using transposition cipher.'|' is added at to
+        indicate end of message.For invalid key size,InvalidKeySizeException
+        is thrown.For empty input message,'|' is returned.
     '''
     message_size = len(message)
-    #the transposition cipher’s key is limited to half the length of the 
-    #message it is used to encrypt
-    if keysize <= message_size//2:
-        encrypted_message = keysize * ['']
-        letter_index = 0
-        while letter_index < message_size:
-            column_index = 0
-            while column_index < keysize and letter_index < message_size:
-                encrypted_message[column_index] += message[letter_index]
-                letter_index += 1
-                column_index += 1
-        encrypted_message = ''.join(encrypted_message)+'|'
-        return encrypted_message
-    else:
-        #print('Key Size is Invalid')       
-        return '|'
+    # the transposition cipher’s key is limited to half the length of the
+    # message it is used to encrypt
+    try:
+        if keysize <= message_size//2:
+            encrypted_message = keysize * ['']
+            letter_index = 0
+            while letter_index < message_size:
+                column_index = 0
+                while column_index < keysize and letter_index < message_size:
+                    encrypted_message[column_index] += message[letter_index]
+                    letter_index += 1
+                    column_index += 1
+            encrypted_message = ''.join(encrypted_message)+'|'
+            return encrypted_message
+        else:
+            if message_size == 0:
+                encrypted_message = '|'
+                return encrypted_message
+            else:
+                # Raise an exception indicating invalid KeySize for
+                # non-empty message
+                raise InvalidKeySizeException(keysize, message_size)
+    except InvalidKeySizeException as err:
+        print(err)
+        return ''
+print(encrypt(5, 'Hello World'))
+print(encrypt(2, ''))
+print(encrypt(5, 'Hello'))
--- tests/transpos_test.py
+++ tests/transpos_test.py
@@ -1,13 +1,22 @@
 """Unit tests for reverse cipher module."""
 
 import unittest
-from libcipher.transpos import encrypt
+#from libcipher.transpos import encrypt
+from transpos import encrypt
+from transpos import InvalidKeySizeException
+
+
 
 class TransposEncryptTestCase(unittest.TestCase):
     def test_keysizeLessthanHalfMessageSize(self):
         encrypted = encrypt(3,'Hello World')
         self.assertEqual(encrypted, "HlWleoodl r|")
-    
-    def test_keysizeGreaterThanHalfMessageSize(Self):
-        encrypted = encrypt(6,'Hello World')
-        #Exception Has to be thrown indicating Key Size Criteria
+    def test_empty(self):
+        encrypted = encrypt(3,'')
+        self.assertEqual(encrypted, "|")
+    def test_keysizeGreaterThanHalfMessageSize(self):
+        with self.assertRaises(InvalidKeySizeException):
+            encrypt(7,'Hello')
+            
+if __name__ == '__main__':
+    unittest.main()
