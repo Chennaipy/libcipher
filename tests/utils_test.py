b901dcffd6fc0018e9a7a89248db7fc47d712afa
Anupama-Github <anupama.2312.bmsit@gmail.com>
2015-05-02 19:23:28 +0530

    Modified code to be compatible with python 3

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -1,33 +1,36 @@
 """Unit tests for english language checker"""
-import inspect, io, os
+import sys
+from libcipher import utils as utils
+import inspect
+import io
+import os
 import unittest
-import libcipher.utils as utils
-
 
 filepath = os.path.dirname(os.path.abspath(inspect.getfile(utils)))
+
+
 def get_marys_dictionary():
         english_words = ["had", "a", "little", "lamb"]
         text = str("\n".join(english_words))
-        dict_file = io.StringIO(u"""\n""".join(english_words))
+        dict_file = io.StringIO("""\n""".join(english_words))
         return dict_file
+
+
 class english_check_testcase(unittest.TestCase):
-    
-    
-       def test_for_english(self):
-         dict_file = get_marys_dictionary()
-         checker = utils.EnglishChecker(dict_file)
-         dict_file.close()    
-         self.assertTrue(checker.is_english("Mary had a little lamb"))
-        
-       
 
-       def test_for_no_letters(self):
-        x = utils.EnglishChecker()
-        self.assertFalse(x.is_english("222 #@#!#<:"))
+        def test_for_english(self):
+            dict_file = get_marys_dictionary()
+            checker = utils.EnglishChecker(dict_file)
+            dict_file.close()
+            self.assertTrue(checker.is_english("Mary had a little lamb"))
 
-       def test_for_non_english(self):
-        x = utils.EnglishChecker()
-        self.assertFalse(x.is_english("Mary avait un petit agneau"))
+        def test_for_no_letters(self):
+            x = utils.EnglishChecker()
+            self.assertFalse(x.is_english("222 #@#!#<:"))
+
+        def test_for_non_english(self):
+            x = utils.EnglishChecker()
+            self.assertFalse(x.is_english("Mary avait un petit agneau"))
 
 if __name__ == '__main__':
     unittest.main()
