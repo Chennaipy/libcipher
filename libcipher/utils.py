3594ca399e6fb94ad5c336fdc8cea5da6fd1d6e0
Anupama-Github <anupama.2312.bmsit@gmail.com>
2015-05-04 09:36:33 +0530

    used package 'six'

--- libcipher/utils.py
+++ libcipher/utils.py
@@ -2,7 +2,7 @@ import io
 import os
 import inspect
 import string
-import sys
+import six
 dictionary_file_path = os.path.dirname(__file__)
 fileobject = open(dictionary_file_path+'/dictionary.txt')
 
@@ -24,10 +24,10 @@ class EnglishChecker():
 
         else:
             self.filename = open(dictionary_file_path+'/dictionary.txt')
-            if sys.version < '3':
-                content = io.StringIO(unicode(self.filename.read())).getvalue()
+            if six.PY2:
+                content = six.StringIO(unicode(self.filename.read())).getvalue()
             else:
-                content = io.StringIO(str(self.filename.read())).getvalue()
+                content = six.StringIO(str(self.filename.read())).getvalue()
             
             self.english_words = {}
             for word in content:
