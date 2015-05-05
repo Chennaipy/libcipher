417f9badeb33f354fef09c271396b0899fcdd812
Anupama-Github <anupama.2312.bmsit@gmail.com>
2015-05-05 11:58:33 +0530

    removed test case that does not use in memory dictionary file and modified test cases to use it

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -1,12 +1,8 @@
 """Unit tests for english language checker"""
 from libcipher import utils as utils
-import inspect
 import io
-import os
 import sys
 import unittest
-
-filepath = os.path.dirname(os.path.abspath(inspect.getfile(utils)))
 
 
 def get_marys_dictionary():
@@ -26,16 +22,18 @@ class english_check_testcase(unittest.TestCase):
             checker = utils.EnglishChecker(dict_file)
             dict_file.close()
             self.assertTrue(checker.is_english("Mary had a little lamb"))
-            x = utils.EnglishChecker()
-            self.assertTrue(x.is_english("Hello my dear"))
 
         def test_for_non_english(self):
-            x = utils.EnglishChecker()
-            self.assertFalse(x.is_english("Mary avait un petit agneau"))
+            dict_file = get_marys_dictionary()
+            checker = utils.EnglishChecker(dict_file)
+            dict_file.close()
+            self.assertFalse(checker.is_english("Mary avait un petit agneau"))
 
         def test_for_no_letters(self):
-            x = utils.EnglishChecker()
-            self.assertFalse(x.is_english("222 #@#!#<:"))
+            dict_file = get_marys_dictionary()
+            checker = utils.EnglishChecker(dict_file)
+            dict_file.close()
+            self.assertFalse(checker.is_english("222 #@#!#<:"))
 
 
 if __name__ == '__main__':
