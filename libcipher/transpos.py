d6e7a695e1831af17eda7f709b7d36e199dc8151
Vijay Kumar <vijaykumar@bravegnu.org>
2015-07-06 23:04:57 +0530

    Merge the two modules for transpose encryption.

--- libcipher/transpos.py
+++ libcipher/transpos.py
@@ -2,23 +2,30 @@
 """Transposition cipher implementation.
 
 Provides functions to encrypt/decrypt a message by transpose ciphering it.
+
+>>> encrypt(8, 'Common sense is not so common.')
+'Cenoonommstmme oo snnio. s s c'
+>>> decrypt(8, 'Cenoonommstmme oo snnio. s s c')
+'Common sense is not so common.'
 """
+
+from __future__ import division
+import math
 
 
 def encrypt(key, message):
     '''Returns the encrypted message.
 
     Args :
-        key(int): key for transposition cipher encryption.
-        message(string):Message to be encrypted using transposition
-          cipher encryption.
+        key(int): the encryption key.
+        message(string): message to be encrypted.
           
     Returns:
         string: the encrypted message
         
     Raises:
-        ValueError: When the key is greater than half
-        of the message size,this exception is raised.
+        ValueError: When the key is greater than half of the message
+        size,this exception is raised.
     '''
     message_size = len(message)
     # the transposition cipher key is limited to half the length of the
@@ -34,3 +41,42 @@ def encrypt(key, message):
                 pointer += key
         encrypted_message = ''.join(encrypted_message)
         return encrypted_message
+
+
+# The Transposition decrypt function will simulate the columns and
+# rows of the grid that the plain text is written on by using a list of
+# strings. No of Columns is calculated by (length of the text)/key.
+# key value is consider as no of rows.
+
+
+def decrypt(key, message):
+    """Returns the decrypted message.
+
+    Args:
+      key (int): decryption key
+      message (str): the encrypted string
+
+    Returns:
+      str: the decrypted message.
+    """
+
+    num_of_columns = int(math.ceil(len(message) / key))
+    num_of_rows = key
+    num_of_shaded_boxes = (num_of_columns * num_of_rows) - len(message)
+
+    plaintext = [''] * num_of_columns
+
+    col = 0
+    row = 0
+
+    for symbol in message:
+        plaintext[col] += symbol
+        col += 1
+
+        if (col == num_of_columns or
+             col == num_of_columns - 1 and
+             row >= num_of_rows - num_of_shaded_boxes):
+            col = 0
+            row += 1
+
+    return ''.join(plaintext)
--- tests/transpos_test.py
+++ tests/transpos_test.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
-"""Unit tests for tranpos cipher module-encrypt function."""
+"""Unit tests for tranpos cipher module."""
 
 import unittest
 from libcipher.transpos import encrypt
+from libcipher.transpos import decrypt
 
 
-class TransposEncryptTestCase(unittest.TestCase):
+class EncryptTestCase(unittest.TestCase):
     """Test cases for transpose encryption."""
     def test_simple(self):
         """Test positive scenario."""
@@ -19,6 +20,13 @@ class TransposEncryptTestCase(unittest.TestCase):
     def test_invalid_key(self):
         """Test ValueError is raise for key size < half message size."""
         self.assertRaises(ValueError, encrypt, 7, 'Hello World')
+
+
+class DecryptTestCase(unittest.TestCase):
+    """Test cases for transpose decryption."""
+    def test_decrypt(self):
+        decrypted = decrypt(8, 'Cenoonommstmme oo snnio. s s c')
+        self.assertEqual(decrypted, 'Common sense is not so common.')
 
 
 if __name__ == '__main__':
